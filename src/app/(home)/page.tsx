a779d75f283e5e10cf9f7a15aebef239f134daaf
nedrazzz <smg9972@gmail.com>
2024-12-02 09:40:31 +0900

    refactor: supabase.ts move to src/lib test: supabase test

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,17 @@
+import { supabase } from "~/src/lib/supabase";
+
 export default function Home() {
+  const addUser = async () => {
+    const { data, error } = await supabase.from("user").insert({
+      name: "user1",
+    });
+
+    if (error) console.log(error);
+    if (data) console.log(data);
+  };
+
+  addUser();
+
   return (
     <div>
       <h1>Home</h1>
